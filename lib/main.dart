bfb69e25e578f01a207c4222c2be6e1f2ea54710
rajput-hemant <hemant.rajput_cs20@gla.ac.in>
2022-05-25 03:23:55 +0530

    Created transaction.dart added dummy list of transactions & added Column Widget for mapping transactions
    Signed-off-by: rajput-hemant <hemant.rajput_cs20@gla.ac.in>

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'models/transaction.dart';
 
 void main() => runApp(MyApp());
 
@@ -13,6 +14,20 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
+  final List<Transaction> transactions = [
+    Transaction(
+      id: 't1',
+      title: 'New Shoes',
+      amount: 69.99,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Weekly Groceries',
+      amount: 16.53,
+      date: DateTime.now(),
+    ),
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,10 +46,13 @@ class MyHomePage extends StatelessWidget {
               elevation: 5,
             ),
           ),
-          Card(
-            color: Colors.blue,
-            child: Text('List of Transactions'),
-          )
+          Column(
+            children: transactions.map(
+              (transaction) {
+                return Card(child: Text(transaction.title));
+              },
+            ).toList(),
+          ),
         ],
       ),
     );
---
+++ lib/models/transaction.dart
@@ -0,0 +1,12 @@
+class Transaction {
+  final String id, title;
+  final double amount;
+  final DateTime date;
+
+  Transaction({
+    required this.id,
+    required this.title,
+    required this.amount,
+    required this.date,
+  });
+}
