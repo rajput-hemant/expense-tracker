b151ffa2b4bddbbbfffa8a7b386c3488fc1a6f10
rajput-hemant <hemant.rajput_cs20@gla.ac.in>
2022-05-26 19:37:17 +0530

    Created chart_bar.dart
    Signed-off-by: rajput-hemant <hemant.rajput_cs20@gla.ac.in>

---
+++ lib/widgets/chart_bart.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class ChartBar extends StatelessWidget {
+  final String label;
+  final double spendingAmount, spendingPercentOfTotal;
+
+  ChartBar(this.label, this.spendingAmount, this.spendingPercentOfTotal);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text('₹ ${spendingAmount.toStringAsFixed(0)}'),
+        SizedBox(
+          height: 4,
+        ),
+        Container(
+          height: 60,
+          width: 10,
+          child: Stack(children: [
+            Container(
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.grey, width: 1.0),
+                color: Color.fromRGBO(220, 220, 220, 1),
+                borderRadius: BorderRadius.circular(10),
+              ),
+            ),
+            FractionallySizedBox(
+              heightFactor: spendingPercentOfTotal,
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Theme.of(context).primaryColor,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+            )
+          ]),
+        )
+      ],
+    );
+  }
+}
