cab66325b9e19ba90c2dc787741652ab9fb629c1
rajput-hemant <hemant.rajput_cs20@gla.ac.in>
2022-05-27 01:59:21 +0530

    Added DatePicker Widget
    Signed-off-by: rajput-hemant <hemant.rajput_cs20@gla.ac.in>

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class NewTransaction extends StatefulWidget {
   final Function addNewTransaction;
@@ -12,14 +13,16 @@ class NewTransaction extends StatefulWidget {
 class _NewTransactionState extends State<NewTransaction> {
   // String? titleInput;
   // String? amountInput;
-  final titleController = TextEditingController();
-  final amountController = TextEditingController();
+  final _titleController = TextEditingController();
+  final _amountController = TextEditingController();
+  DateTime? _selectedDate;
 
-  void submitData() {
-    final enteredTitle = titleController.text;
-    final enteredAmount = double.parse(amountController.text);
+  void _submitData() {
+    if (_amountController.text.isEmpty) return;
+    final enteredTitle = _titleController.text;
+    final enteredAmount = double.parse(_amountController.text);
 
-    if (enteredTitle.isEmpty || enteredAmount <= 0) {
+    if (enteredTitle.isEmpty || enteredAmount <= 0 || _selectedDate == null) {
       return;
     }
 
@@ -31,6 +34,18 @@ class _NewTransactionState extends State<NewTransaction> {
     Navigator.of(context).pop();
   }
 
+  void _presentDatePicker() {
+    showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime(2019),
+      lastDate: DateTime.now(),
+    ).then((pickedDate) {
+      if (pickedDate == null) return;
+      setState(() => _selectedDate = pickedDate);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -39,25 +54,54 @@ class _NewTransactionState extends State<NewTransaction> {
         child: Container(
           padding: EdgeInsets.all(10),
           child: Column(
-            crossAxisAlignment: CrossAxisAlignment.end,
+            // crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
+              Center(
+                child: Text('New Transaction',
+                    style: Theme.of(context).textTheme.titleLarge),
+              ),
               TextField(
                 decoration: InputDecoration(labelText: 'Enter the Title'),
                 // onChanged: (value) => titleInput = value,
-                controller: titleController,
-                onSubmitted: (_) => submitData(),
+                controller: _titleController,
+                onSubmitted: (_) => _submitData(),
               ),
               TextField(
                 decoration: InputDecoration(labelText: 'Enter the Amount'),
                 // onChanged: (value) => amountInput = value,
-                controller: amountController,
+                controller: _amountController,
                 // keyboardType: TextInputType.number,
-                onSubmitted: (_) => submitData(),
+                onSubmitted: (_) => _submitData(),
               ),
-              FlatButton(
+              Container(
+                height: 80,
+                child: Row(
+                  children: [
+                    // ignore: unnecessary_null_comparison
+                    Expanded(
+                        child: Text(_selectedDate == null
+                            ? 'No Date Chosen!'
+                            : 'Picked Date: ${DateFormat.yMd().format(_selectedDate!)}')),
+                    FlatButton(
+                      // textColor: Theme.of(context).primaryColor,
+                      child: Text(
+                        'Choose Date',
+                        // style: TextStyle(fontWeight: FontWeight.bold)
+                        style: Theme.of(context).textTheme.headline6,
+                      ),
+                      onPressed: _presentDatePicker,
+                    ),
+                  ],
+                ),
+              ),
+              ElevatedButton(
+                onPressed: _submitData,
                 child: Text('Add Transaction'),
-                textColor: Colors.purple,
-                onPressed: submitData,
+                style: ElevatedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                    textStyle: Theme.of(context).textTheme.headline6),
               )
             ],
           ),
